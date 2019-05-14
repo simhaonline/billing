11ec73d829ba110d1bf288c9aa21175aff80f383
vinxstudio <vinxstudio@gmail.com>
2019-05-14 21:58:48 +1000

    Update manifest.php

--- application/plugins/notes/manifest.php
+++ application/plugins/notes/manifest.php
@@ -3,5 +3,5 @@ $plugin = array();
 $plugin['name'] = 'Note';
 $plugin['author'] = 'Your Name';
 $plugin['version'] = '1.0.0';
-$plugin['description'] = 'Sample Plugin for iBilling';
-$plugin['url'] = 'http://www.ibilling.io/plugin-development/';
+$plugin['description'] = 'Sample Plugin';
+$plugin['url'] = '';
