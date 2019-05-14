9cd2083ccda60fc804521a446ed245188bb3ccf8
vinxstudio <vinxstudio@gmail.com>
2019-05-14 21:56:55 +1000

    Update application_installer_config.php

--- application/install/application_installer_config.php
+++ application/install/application_installer_config.php
@@ -1,8 +1,8 @@
 <?php
 
-$app_name = 'iBilling';
-$app_url = 'www.ibilling.io';
+$app_name = 'Billing';
+$app_url = '';
 $release_date = '20th March 2015';
-$envato_username = 'SadiaSharmin';
-$author_name = 'Sadia Sharmin';
-$support_url = 'http://codecanyon.net/user/SadiaSharmin';
+$envato_username = '';
+$author_name = '';
+$support_url = '';
